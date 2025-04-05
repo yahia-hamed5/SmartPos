3df619061d6acf1514395bb666457a480afd6c1b
yahia-hamed5 <yahiahamed2411@gmail.com>
2025-04-05 18:44:45 +0200

    Edit regex name

--- assets/js/regex.js
+++ assets/js/regex.js
@@ -1,3 +1,3 @@
-const nemeRegex=/^[a-zA-Z]{4,20}$/;
+const nemeRegex=/^(?=.{2,25}$)(?:[^\s]+\s+[^\s]+)+$/;
 const emailRegex=/^[^@]+@[^@]+\.[^@]+$/;
 const passwordRegex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,20}$/;
